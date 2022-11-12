30ac72a6c2de5ca0d1a80845c6f3b8d0f9366088
jiawong123 <jiawongms@outlook.com>
2022-11-12 11:08:16 +0800

    Fix the path name in cmd  with space

--- codes/pipeline_scripts/QC_tools/setData_anonymize.m
+++ codes/pipeline_scripts/QC_tools/setData_anonymize.m
@@ -8,24 +8,28 @@
 %
 % Jia Wang @ SJTU
 % 13 Oct 2022
+%
+% Modified (12 NOV 2022)
+% Fix the path name in `cmd` with space.
 
 clc, clear, close all
 
 %% initialization
 
 % ----- set path ----------------------------------------------------------
-dataDir = '/data/share/Epilepsy_SPICE_20221011/';            % path of the data
-processFolder = '/data/share/temp/Epilepsy_SPICE_20221011/'; % path of the data after anony
+parentFolderName = 'Epilepsy_20221112';
+dataDir = fullfile('/data/share', parentFolderName);            % path of the data
+processFolder = fullfile('/data/share/temp', parentFolderName); % path of the data after anony
 
-pythonPath = '/home/jiawong/anaconda3/bin/python3';          % path of the python complier
-pyfilePath = 'dat_anonymizer.py';                            % path of the python file
+pythonPath = '/home/jiawong/anaconda3/bin/python3';             % path of the python complier
+pyfilePath = 'dat_anonymizer.py';                               % path of the python file
 
 % ----- set the parameters for data transfer ------------------------------
-host = '127.0.0.1';  % IP host
+host = '120.0.0.1';  % IP host
 port = '22';         % Port number
-user = 'user';       % username
-passwd = 'passwd.,'; % Password
-tarDir = '/data/';   % Folder on the target server to save the data
+user = 'username';   % username
+passwd = 'passwd';   % Password
+toDir = '/data/';    % Folder on the target server to save the data
 
 %% set the path
 
@@ -72,7 +76,7 @@ for idx =1:length(dataIDList)
                 filename = SpiceDataScanName{j};
                 renamefile = fullfile(tarDir,['anony_',filename(strfind(filename,'meas'):end)]);
 
-                cmd = sprintf('%s %s --dir %s --filename %s --targetname %s', ...
+                cmd = sprintf('''%s'' ''%s'' --dir ''%s'' --filename ''%s'' --targetname ''%s''', ...
                                 pythonPath, ...
                                 pyfilePath, ...
                                 scanDir, ...
@@ -89,17 +93,23 @@ end
 
 %% transfer data
 
-for idx =1:length(dataIDList)
-    % data information
-    dataIDFullName     = dataIDList{idx};
-    dataID             = strsplit(dataIDFullName, '_');
-    dataID             = dataID{1};
-    
-    subDir = fullfile(processFolder, dataID);
-    
-    cmd = sprintf('sshpass -p %s scp -r -P %s %s %s@%s:%s', passwd, port, subDir, user, host, tarDir);
-    fprintf('Start transfer %s...', dataID)
-    unix(cmd)
-    fprintf('done.\n')
-    
-end
+% for idx =1:length(dataIDList)
+%     % data information
+%     dataIDFullName     = dataIDList{idx};
+%     dataID             = strsplit(dataIDFullName, '_');
+%     dataID             = dataID{1};
+%     
+%     subDir = fullfile(processFolder, dataID);
+%     tarDir = fullfile(toDir, parentFolderName, dataID);
+%     
+%     cmd = sprintf('sshpass -p %s scp -r -P %s ''%s'' %s@%s:''%s''', passwd, port, subDir, user, host, tarDir);
+%     fprintf('Start transfer %s...', dataID)
+%     unix(cmd)
+%     fprintf('done.\n')
+%     
+% end
+
+cmd = sprintf('sshpass -p %s scp -r -P %s ''%s'' %s@%s:''%s''', passwd, port, processFolder, user, host, toDir);
+fprintf('Start transfer ...')
+unix(cmd)
+fprintf('done.\n')
